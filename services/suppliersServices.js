f4494c8e228c4db53c053c613fc937a39520e40d
Marynryn <132609093+Marynryn@users.noreply.github.com>
2024-07-11 23:50:18 +0300

    Update suppliersServices.js

--- services/suppliersServices.js
+++ services/suppliersServices.js
@@ -1,8 +1,8 @@
 import HttpError from "../helpers/HttpError.js";
 import { Supplier } from "../model/supplierModel.js";
 
-export const fetchSuppliers = () => {
-  return Supplier.find();
+export const fetchSuppliers= (filterQuery = {}) => {
+  return Supplier.find(filterQuery);
 };
 export const checkSupplierExists = async (filter, throwError = true) => {
   const supplierExists = await Supplier.exists(filter);
